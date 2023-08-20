c66c4fc58a877e5d5387273e9b3b11927227d918
arcticzombieyt <zszombiesize@gmail.com>
2023-08-20 00:02:19 -0400

    Update lesson and speech recog

--- src/components/Lesson.jsx
+++ src/components/Lesson.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from 'react'
 import { Text, Box, VStack, Button } from '@chakra-ui/react';
 import { FaMicrophone, FaCheck } from "react-icons/fa";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import SpeechRecognitionGoogleAPI from './SpeechRecognitionGoogleAPI';
+
 function Lesson() {
     const [bgColour, setBgColour] = useState("green.100");
+    const [audioFilePath, setAudioFilePath] = useState('');
+    const textPrompt = "LESSON TEXT";
 
     return (
         <Box display="flex" justifyContent="center" alignItems="center" height="80vh" width="100%">
@@ -17,17 +21,20 @@ function Lesson() {
                     width="80%"
                 >
                     <Text fontSize="3xl" fontWeight="bold" textAlign="center">
-                        LESSON TEXT
+                        {textPrompt}
                     </Text>
                 </Box>
-                <Button w="200px" h="100px" bg={bgColour} className="hover-rise" onClick={() => bgColour == "green.100" ? setBgColour("red.100") : setBgColour("green.100")}>
+                <Button w="200px" h="100px" bg={bgColour} className="hover-rise" onClick={() => bgColour === "green.100" ? setBgColour("red.100") : setBgColour("green.100")}>
                     <FaMicrophone />
                 </Button>
                 <Link to="/results">
-                <Button w="200px" h="50px" bg="green.500">
-                    <FaCheck />
-                </Button>
+                    <Button w="200px" h="50px" bg="green.500">
+                        <FaCheck />
+                    </Button>
                 </Link>
+                
+                {/* Integrate the Speech Recognition Component */}
+                <SpeechRecognitionGoogleAPI audioFilePath={audioFilePath} textPrompt={textPrompt} />
             </VStack>
         </Box>
     );
