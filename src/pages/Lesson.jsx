5e5427711647ac2b8db025fd7e8a5832997abc76
Taisir Hassan <85134103+taisirhassan@users.noreply.github.com>
2023-08-19 23:02:19 -0400

    Added User Authentication using Firebase

--- src/pages/Lesson.jsx
+++ src/pages/Lesson.jsx
@@ -2,39 +2,50 @@ import React, { useState, useEffect } from 'react'
 import { Text, Box, VStack, Button } from '@chakra-ui/react';
 import { FaMicrophone, FaCheck } from "react-icons/fa";
 import {Link} from "react-router-dom";
-import axios from 'axios';
+
+const generate = async () => {
+  const key = "uMOsOiLKyUjoJL1fIoqVUhUiveizJt2Qdslj64Bw";
+  const options = {
+    method: "POST",
+    headers: {
+      accept: "application/json",
+      "content-type": "application/json",
+      authorization: `Bearer ${key}`,
+    },
+
+    body: JSON.stringify({
+      model: "command",
+      prompt: "Generate a random cohesive sentence everytime, don't put a prefacing sentence",
+      temperature: 0,
+      max_tokens: 3000,
+    }),
+  };
+
+  try {
+    const response = await fetch("https://api.cohere.ai/v1/generate", options);
+    return response.json();
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}; 
 function Lesson() {
-const [generatedText, setGeneratedText] = useState('');
-
-useEffect(() => {
-    const options = {
-      method: 'POST',
-      url: 'https://api.cohere.ai/v1/generate',
-      headers: {
-        accept: 'application/json',
-        'content-type': 'application/json',
-        authorization: 'Bearer uMOsOiLKyUjoJL1fIoqVUhUiveizJt2Qdslj64Bw'
-      },
-      data: {
-        max_tokens: 20,
-        truncate: 'END',
-        return_likelihoods: 'NONE',
-        prompt: 'Please explain to me how LLMs work'
-      }
-    };
-
-    axios
-      .request(options)
-      .then(function (response) {
-        setGeneratedText(response.data.choices[0].text);
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
-  }, []);
-
     const [bgColour, setBgColour] = useState("green.100");
+    const [generatedText, setGeneratedText] = useState("");
 
+    useEffect(() => {
+      generate().then((response) => {
+        const text = response.generations[0].text.trim();
+        setGeneratedText(text);
+      });
+    }, []);
+
+const handleGenerate = () => {
+    generate().then((response) => {
+      const text = response.generations[0].text.trim();
+      setGeneratedText(text);
+    });
+  };
     return (
         <Box display="flex" justifyContent="center" alignItems="center" height="80vh" width="100%">
             <VStack spacing={6}>
@@ -47,7 +58,7 @@ useEffect(() => {
                     width="80%"
                 >
                     <Text fontSize="3xl" fontWeight="bold" textAlign="center">
-                        {generatedText}
+                       {generatedText}
                     </Text>
                 </Box>
                 <Button w="200px" h="100px" bg={bgColour} className="hover-rise" onClick={() => bgColour == "green.100" ? setBgColour("red.100") : setBgColour("green.100")}>
