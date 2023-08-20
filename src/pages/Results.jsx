141180e824ed926b525241de8d4e67b36b9c583f
arcticzombieyt <zszombiesize@gmail.com>
2023-08-19 20:24:17 -0400

    Update results page with new clock icon

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import {Chart as ChartJS, ArcElement, Tooltip, Legend} from 'chart.js';
-
 import { Doughnut } from 'react-chartjs-2';
+import { Box, Text, VStack, Badge, Button, HStack, Divider} from '@chakra-ui/react';
+import { FaClock } from 'react-icons/fa';
+import {Link} from 'react-router-dom';
+
 ChartJS.register(
     ArcElement,
     Tooltip,
     Legend
 );
-import { Box, Text, VStack, Badge } from '@chakra-ui/react';
-
 function Results() {
   const correctAnswers = 8;
   const totalQuestions = 10; 
-
+  const accuracy = (correctAnswers/totalQuestions) * 100;
+  const timeData = 398;
   const data = {
     labels: ['Correct', 'Incorrect'],
     datasets: [{
@@ -25,19 +27,61 @@ function Results() {
   const options = {
     responsive: true,
     maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        position: 'bottom'
+      }
+    }
   };
+  
+
+  
 
   return (
-    <VStack spacing={8} align="center" mt={6}>
+    <VStack spacing={4} align="center" mt={6}>
+      <HStack spacing={8} align="top" mt={6}>
+      <VStack>
       <Text fontSize="2xl" fontWeight="bold">
-        RESULTS
+        Accuracy: {accuracy}%
       </Text>
       <Box width="300px" height="300px">
         <Doughnut data={data} options={options} />
       </Box>
-      <Text fontSize="lg">
-        You got <Badge colorScheme="green" fontSize="lg">{correctAnswers}</Badge> out of <Badge colorScheme="gray" fontSize="lg">{totalQuestions}</Badge> questions right!
+      </VStack>
+      <VStack>
+      <Text fontSize="2xl" fontWeight="bold">
+        Total time: {timeData}s
       </Text>
+      <Box width="300px" height="270px" display="flex" alignItems="center" justifyContent="center">
+            <FaClock size="280px" />
+          </Box>
+      </VStack>
+      </HStack>
+      <Divider/>
+      <Text fontSize="lg">
+        You got <Badge colorScheme="green" fontSize="lg">{correctAnswers}</Badge> out of <Badge colorScheme="gray" fontSize="lg">{totalQuestions}</Badge> words right!
+      </Text>
+      <Text fontSize="lg">
+        You got <Badge colorScheme="green" fontSize="lg">{correctAnswers}</Badge> coins!
+      </Text>
+      <Divider/>
+      <br/>
+      <Button className="hover-rise"
+              colorScheme="blue"
+              size="lg"
+              shadow="md">
+        Submit Results
+      </Button>
+      <Link to='/lesson'>
+      <Button
+              className="hover-rise"
+              variant="outline"
+              colorScheme="blue"
+              size="lg"
+            >
+        Go back
+      </Button>
+      </Link>
     </VStack>
   )
 }
